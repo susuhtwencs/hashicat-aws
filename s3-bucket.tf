56aab27ce66fa8784e2c52a0463b530556182e0f
susuhtwencs <susuhtwencs@github.com>
2023-06-02 14:12:04 +0000

    Added s3 bucket module

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -1,8 +1,5 @@
-module "s3-bucket" {
-  source  = "app.terraform.io/ncsit/s3-bucket/aws"
+module "terraform-aws-s3-bucket" {
+  source  = "app.terraform.io/ncsit/terraform-aws-s3-bucket/aws"
   version = "2.8.0"
 }
-variable "bucket_prefix" {
-  type        = string
-  description = "susuhtwe"
-}
+
